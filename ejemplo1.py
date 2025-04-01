2c399b3b90585f53da2ea77e9574c992cfe58c07
redshadow18 <1001217419@teinco.edu.co>
2025-04-01 09:11:25 -0500

    Create ejemplo1.py

---
+++ ejemplo1.py
@@ -0,0 +1,24 @@
+#creacion superclase animal
+class Animal:
+  def sonido (self):
+    pass
+
+class perro(Animal):
+  def sonido(self):
+    return "guau"
+
+
+class gato(Animal):
+  def sonido(self):
+    return "miau"
+
+hacer_sonido(animal:Animal):
+     print(animal.sonido())
+
+perro=perro()
+gato=gato()
+
+hacer_sonido(perro)
+hacer_sonido(gato)
+
+
